f4f594cd4417ed99e6f9eefc0c08d9781b082b4a
zelda101 <zeldamob2@gmai.com>
2022-04-07 18:39:32 -0500

    updates

---
+++ commands/mark.js
@@ -0,0 +1,16 @@
+const { shigeapiClient } = require("../utils/apiclient")
+
+module.exports = {
+	name: "mark",
+	description: "Creates a stream marker.",
+	canWhisper: false,
+	modOnly: true, 
+	execute: async function(channel, user, msg, context, chatClient, data) {
+		user = await shigeapiClient.users.getUserByName("shigetora")
+		const description = msg.replace("!mark", "") 
+		console.log(description)
+		if (description) await shigeapiClient.streams.createStreamMarker(user, description)
+		else await shigeapiClient.streams.createStreamMarker(user)
+		chatClient.say(channel, "Stream marker set.")
+	}
+}
