77b4c91e1fbaee3d0bee31ba8611fa50ea428596
zelda101 <zeldamob2@gmai.com>
2022-06-01 16:42:14 -0500

    test

--- commands/roll.js
+++ commands/roll.js
@@ -4,9 +4,11 @@ module.exports = {
 	canWhisper: true,
 	isOsuCommand: false,
 	execute: async function(channel, user, msg, context, chatClient, data) {
+		var numberToRoll = msg.split(" ").filter(item => item)
+		numberToRoll = numberToRoll[1]
 		var maxDice = 6
 		console.log(msg.split(" "))
-		if (!isNaN(msg.split(" ")[1]) && msg.split(" ")[1] > 1) maxDice = msg.split(" ")[1]
+		if (!isNaN(numberToRoll) && msg.split(" ")[1] > 0) maxDice = numberToRoll
 		var randomDice = Math.floor(maxDice*Math.random()) + 1
 		chatClient.say(channel, randomDice)
 	}
