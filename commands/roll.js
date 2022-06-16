0cb39bd282373d3f2aaf534f995f93502f9672e9
zelda101 <zeldamob2@gmai.com>
2022-06-16 18:15:16 -0500

    wegtaefhdg

--- commands/roll.js
+++ commands/roll.js
@@ -3,11 +3,10 @@ module.exports = {
 	description: "Roll a dice.",
 	canWhisper: true,
 	isOsuCommand: false,
+	modOnly: true, 
 	execute: async function(channel, user, msg, context, chatClient, data) {
 		var numberToRoll = msg.split(" ").filter(item => item)
-		console.log(numberToRoll)
 		numberToRoll = numberToRoll[1]
-		console.log(numberToRoll)
 		var maxDice = 6
 		if (!isNaN(numberToRoll) && numberToRoll > 0) maxDice = numberToRoll
 		var randomDice = Math.floor(maxDice*Math.random()) + 1
--- commands/subcount.js
+++ commands/subcount.js
@@ -9,6 +9,7 @@ module.exports = {
 	execute: async function(channel, user, msg, context, chatClient, data) {
 		user = await shigeapiClient.users.getUserByName("shigetora")
 		const subs = await shigeapiClient.subscriptions.getSubscriptions(user)
+		console.log(subs)
 		chatClient.say(channel, subs.total)
 	}
 }
