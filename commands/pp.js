b64efefc01421f7bfaf1a73dc05072a515afe53d
zelda101 <zeldamob2@gmai.com>
2022-10-15 21:00:38 -0500

    test2

--- commands/pp.js
+++ commands/pp.js
@@ -20,6 +20,7 @@ module.exports = {
 				var msgArray = msg.split(" ")
 				for (var i = 0; i < msgArray.length; i++) {
 					if (msgArray[i].indexOf("+") > -1) {
+						console.log(i)
 						mods = msgArray[i].substring(1)
 						if (i == 2) accuracy = Number(msg.toLowerCase().split(" ")[2]).toFixed(2)
 						else accuracy = Number(msg.toLowerCase().split(" ")[1]).toFixed(2)
