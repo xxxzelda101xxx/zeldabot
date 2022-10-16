5d9b1eb25d05c4fc777736515bf5547432e61615
zelda101 <zeldamob2@gmai.com>
2022-10-15 21:35:46 -0500

    acc

--- commands/pp.js
+++ commands/pp.js
@@ -14,14 +14,15 @@ module.exports = {
 				chatClient.whisper(user, `${pp}pp`)
 			}
 		}
-		else if (msg.toLowerCase().split(" ").length > 1) {
+		else if (msg.toLowerCase().split(" ").length > 1 && msg.toLowerCase().split(" ").length < 4) {
 			var mods = "", accuracy
 			if (msg.indexOf("+") > -1) {
 				var msgArray = msg.split(" ")
 				for (var i = 0; i < msgArray.length; i++) {
 					if (msgArray[i].indexOf("+") > -1) {
 						mods = msgArray[i].substring(1).toLowerCase()
-						if (i == 1) accuracy = Number(msg.toLowerCase().split(" ")[2]).toFixed(2)
+						if (msgArray.length == 2) accuracy = 100
+						else if (i == 1) accuracy = Number(msg.toLowerCase().split(" ")[2]).toFixed(2)
 						else accuracy = Number(msg.toLowerCase().split(" ")[1]).toFixed(2)
 					}
 				}
@@ -35,6 +36,14 @@ module.exports = {
 			}
 			else {
 				chatClient.whisper(user, `${pp}pp for a ${accuracy}% (${count100}x100) ${mods.toUpperCase()} fc.`)
+			}
+		}
+		else {
+			if (channel) {
+				chatClient.say(channel, "Invalid command usage. Example usage: \"!pp +HDHR 100")
+			}
+			else {
+				chatClient.whisper(user, "Invalid command usage. Example usage: \"!pp +HDHR 100")
 			}
 		}
 	}
