ca2cb8e4eb4998ca52eb9ca67617d04d2b78b0e6
zelda101 <zeldamob2@gmail.com>
2022-10-18 17:35:50 -0500

    ok

--- handlers/messagehandler.js
+++ handlers/messagehandler.js
@@ -19,7 +19,6 @@ async function messageHandler(channel, user, msg, context) {
 	const commandToRun = Commands[command]
 	const isMod = (context.userInfo.isMod || context.userInfo.isBroadcaster) ? true : false
 	const whitelistStatus = await getWhitelistStatus(user_id)
-	console.log(whitelistStatus, !whitelistStatus)
 	if (commandToRun) {
 		if (!commandToRun.isPublic && !whitelistStatus) return
 		if (commandToRun.adminOnly && admins.indexOf(user) < 0) return
