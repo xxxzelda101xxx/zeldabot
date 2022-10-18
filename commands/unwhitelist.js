a0c0bd88b57a1670ededb09471731ec296edf1e4
zelda101 <zeldamob2@gmail.com>
2022-10-18 17:01:37 -0500

    dded

--- commands/unwhitelist.js
+++ commands/unwhitelist.js
@@ -11,7 +11,7 @@ module.exports = {
 	execute: async function(channel, user, msg, context, chatClient, data) {
         user_id = await getUserIdByUsername(msg.toLowerCase().split(" ")[1])
         if (user_id) {
-            await unwhitelist(user_id)
+            await unwhitelistUser(user_id)
             chatClient.say(channel, `${msg.toLowerCase().split(" ")[1]} has been removed from the whitelist.`)
         }
         else {
