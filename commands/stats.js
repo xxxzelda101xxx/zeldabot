fecf64d74a7bb014b2d3a5aa26493239ac459e84
zelda101 <zeldamob2@gmail.com>
2022-10-20 01:48:15 -0500

    yep

--- commands/stats.js
+++ commands/stats.js
@@ -1,3 +1,6 @@
+const { BeatmapCalculator } = require("@kionell/osu-pp-calculator")
+const beatmapCalculator = new BeatmapCalculator();
+
 module.exports = {
 	name: "stats",
 	aliases: [],
@@ -6,12 +9,24 @@ module.exports = {
 	isOsuCommand: true,
 	isPublic: false,
 	execute: async function(channel, user, msg, context, chatClient, data) {
-		var newSR = await data.getNewSR()
-		if (channel) {
-			chatClient.say(channel, `New SR: ${newSR}★, Old SR: ${data.getSR()}★, ${data.getLength()}, ${data.getBpm()}bpm,  AR${data.getAR()}, CS${data.getCS()}, OD${data.getOD()}, HP${data.getHP()}`)
+		if (data.menu.bm.rankedStatus >= 2) {
+			var currentStats = data.getCurrentStats()
+			const result = await beatmapCalculator.calculate({ rulesetId: 0, beatmapId: data.beatmap_id, mods: currentStats.leaderboard.ourplayer.mods, accuracy: [] })
+			console.log(result)
+			var newSR = result.difficulty.starRating.toFixed(2)
+			if (channel) {
+				chatClient.say(channel, `SR: ${newSR}★, ${data.getLength()}, ${data.getBpm()}bpm,  AR${data.getAR()}, CS${data.getCS()}, OD${data.getOD()}, HP${data.getHP()}`)
+			}
+			else {
+				chatClient.whisper(user, `New SR: ${newSR}★, ${data.getLength()}, ${data.getBpm()}bpm,  AR${data.getAR()}, CS${data.getCS()}, OD${data.getOD()}, HP${data.getHP()}`)
+			}
 		}
-		else {
-			chatClient.whisper(user, `New SR: ${newSR}★, Old SR: ${data.getSR()}★, ${data.getLength()}, ${data.getBpm()}bpm,  AR${data.getAR()}, CS${data.getCS()}, OD${data.getOD()}, HP${data.getHP()}`)
-		}
+		//var newSR = await data.getNewSR()
+		//if (channel) {
+		//	chatClient.say(channel, `New SR: ${newSR}★, Old SR: ${data.getSR()}★, ${data.getLength()}, ${data.getBpm()}bpm,  AR${data.getAR()}, CS${data.getCS()}, OD${data.getOD()}, HP${data.getHP()}`)
+		//}
+		//else {
+		//	chatClient.whisper(user, `New SR: ${newSR}★, Old SR: ${data.getSR()}★, ${data.getLength()}, ${data.getBpm()}bpm,  AR${data.getAR()}, CS${data.getCS()}, OD${data.getOD()}, HP${data.getHP()}`)
+		//}
 	}
 }
