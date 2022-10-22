7a73c589baed99b77963a2e8a7026bcafd6a7cdd
zelda101 <zeldamob2@gmail.com>
2022-10-22 12:32:54 -0500

    update

--- commands/current.js
+++ commands/current.js
@@ -9,11 +9,26 @@ module.exports = {
 	isOsuCommand: true,
 	isPublic: false,
 	execute: async function(channel, user, msg, context, chatClient, data) {
-		if (data.menu.bm.rankedStatus >= 2) {
+		if ((data.menu.state == 2 || data.menu.state == 7) && data.menu.bm.rankedStatus >= 2) {
 			var currentStats = data.getCurrentStats()
 			const result1 = await scoreCalculator.calculate({ rulesetId: 0, beatmapId: data.beatmap_id, count100: currentStats.hits["100"], count50: currentStats.hits["50"], countMiss: currentStats.hits["0"], maxCombo: currentStats.combo.max, mods: currentStats.leaderboard.ourplayer.mods })
 			const result2 = await scoreCalculator.calculate({ rulesetId: 0, beatmapId: data.beatmap_id, count100: currentStats.hits["100"] + currentStats.hits["0"], count50: currentStats.hits["50"], mods: currentStats.leaderboard.ourplayer.mods })
 			const result3 = await scoreCalculator.calculate({ rulesetId: 0, beatmapId: data.beatmap_id, mods: currentStats.leaderboard.ourplayer.mods })
+			currentPP = result1.performance.totalPerformance.toFixed(2)
+			fcPP = result2.performance.totalPerformance.toFixed(2)
+			ssPP = result3.performance.totalPerformance.toFixed(2)
+			if (channel) {
+				chatClient.say(channel, `${currentStats.hits["100"]}x100/${currentStats.hits["50"]}x50/${currentStats.hits["0"]}xmiss ${parseInt(currentPP)}pp/${parseInt(fcPP)}pp if fc (${parseInt(ssPP)}pp for SS)`)
+			}
+			else {
+				chatClient.whisper(user, `${currentStats.hits["100"]}x100/${currentStats.hits["50"]}x50/${currentStats.hits["0"]}xmiss ${parseInt(currentPP)}pp/${parseInt(fcPP)}pp if fc (${parseInt(ssPP)}pp for SS)`)
+			}
+		}
+		else if ((data.menu.state == 2 || data.menu.state == 7) && data.beatmap_id > 0) {
+			var currentStats = data.getCurrentStats()
+			const result1 = await scoreCalculator.calculate({ rulesetId: 0, beatmapId: data.beatmap_id, count100: currentStats.hits["100"], count50: currentStats.hits["50"], countMiss: currentStats.hits["0"], maxCombo: currentStats.combo.max, mods: currentStats.leaderboard.ourplayer.mods, bpm: data.getBpm(), approachRate: data.getAR(), overallDifficulty: data.getOD(), circleSize: data.getCS(), lockStats: true })
+			const result2 = await scoreCalculator.calculate({ rulesetId: 0, beatmapId: data.beatmap_id, count100: currentStats.hits["100"] + currentStats.hits["0"], count50: currentStats.hits["50"], mods: currentStats.leaderboard.ourplayer.mods, bpm: data.getBpm(), approachRate: data.getAR(), overallDifficulty: data.getOD(), circleSize: data.getCS(), lockStats: true })
+			const result3 = await scoreCalculator.calculate({ rulesetId: 0, beatmapId: data.beatmap_id, mods: currentStats.leaderboard.ourplayer.mods, bpm: data.getBpm(), approachRate: data.getAR(), overallDifficulty: data.getOD(), circleSize: data.getCS(), lockStats: true })
 			currentPP = result1.performance.totalPerformance.toFixed(2)
 			fcPP = result2.performance.totalPerformance.toFixed(2)
 			ssPP = result3.performance.totalPerformance.toFixed(2)
