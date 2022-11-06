f55e736d249c26133e43ef84995103f934ac7eea
zelda101 <zeldamob2@gmai.com>
2022-11-06 14:56:19 -0600

    test

--- commands/current.js
+++ commands/current.js
@@ -9,31 +9,15 @@ module.exports = {
 	isOsuCommand: true,
 	isPublic: false,
 	execute: async function(channel, user, msg, context, chatClient, data) {
-		if ((data.menu.state == 2 || data.menu.state == 7) && data.menu.bm.rankedStatus >= 5) {
+		if (data.menu.state == 2 || data.menu.state == 7) {	
 			var currentStats = data.getCurrentStats()
-			const result1 = await scoreCalculator.calculate({ rulesetId: 0, beatmapId: data.beatmap_id, count100: currentStats.hits["100"], count50: currentStats.hits["50"], countMiss: currentStats.hits["0"], maxCombo: currentStats.combo.max, mods: currentStats.leaderboard.ourplayer.mods })
-			const result2 = await scoreCalculator.calculate({ rulesetId: 0, beatmapId: data.beatmap_id, count100: currentStats.hits["100"] + currentStats.hits["0"], count50: currentStats.hits["50"], mods: currentStats.leaderboard.ourplayer.mods })
-			const result3 = await scoreCalculator.calculate({ rulesetId: 0, beatmapId: data.beatmap_id, mods: currentStats.leaderboard.ourplayer.mods })
-			currentPP = result1.performance.totalPerformance.toFixed(2)
-			fcPP = result2.performance.totalPerformance.toFixed(2)
-			ssPP = result3.performance.totalPerformance.toFixed(2)
-			return `${currentStats.hits["100"]}x100/${currentStats.hits["50"]}x50/${currentStats.hits["0"]}xmiss ${parseInt(currentPP)}pp/${parseInt(fcPP)}pp if fc (${parseInt(ssPP)}pp for SS)`
-		}
-		else if ((data.menu.state == 2 || data.menu.state == 7) && data.beatmap_id > 0) {
-			var currentStats = data.getCurrentStats()
-			const result1 = await scoreCalculator.calculate({ rulesetId: 0, beatmapId: data.beatmap_id, count100: currentStats.hits["100"], count50: currentStats.hits["50"], countMiss: currentStats.hits["0"], maxCombo: currentStats.combo.max, mods: currentStats.leaderboard.ourplayer.mods, bpm: data.getBpm(), approachRate: data.getAR(), overallDifficulty: data.getOD(), circleSize: data.getCS(), lockStats: true })
-			const result2 = await scoreCalculator.calculate({ rulesetId: 0, beatmapId: data.beatmap_id, count100: currentStats.hits["100"] + currentStats.hits["0"], count50: currentStats.hits["50"], mods: currentStats.leaderboard.ourplayer.mods, bpm: data.getBpm(), approachRate: data.getAR(), overallDifficulty: data.getOD(), circleSize: data.getCS(), lockStats: true })
-			const result3 = await scoreCalculator.calculate({ rulesetId: 0, beatmapId: data.beatmap_id, mods: currentStats.leaderboard.ourplayer.mods, bpm: data.getBpm(), approachRate: data.getAR(), overallDifficulty: data.getOD(), circleSize: data.getCS(), lockStats: true })
-			currentPP = result1.performance.totalPerformance.toFixed(2)
-			fcPP = result2.performance.totalPerformance.toFixed(2)
-			ssPP = result3.performance.totalPerformance.toFixed(2)
-			return `${currentStats.hits["100"]}x100/${currentStats.hits["50"]}x50/${currentStats.hits["0"]}xmiss ${parseInt(currentPP)}pp/${parseInt(fcPP)}pp if fc (${parseInt(ssPP)}pp for SS)`
-		}
-		else {
-			var currentStats = data.getCurrentStats()
-			var currentPP = await data.getCurrentPP()
-			var fcPP = await data.getPPIfFc()
-			var ssPP = await data.getSSPP()
+			var osuFile = await data.getOsuFile()
+			const result1 = await scoreCalculator.calculate({ rulesetId: 0, fileURL: osuFile, count100: currentStats.hits["100"], count50: currentStats.hits["50"], countMiss: currentStats.hits["0"], maxCombo: currentStats.combo.max, mods: currentStats.leaderboard.ourplayer.mods })
+			const result2 = await scoreCalculator.calculate({ rulesetId: 0, fileURL: osuFile, count100: currentStats.hits["100"] + currentStats.hits["0"], count50: currentStats.hits["50"], mods: currentStats.leaderboard.ourplayer.mods })
+			const result3 = await scoreCalculator.calculate({ rulesetId: 0, fileURL: osuFile, mods: currentStats.leaderboard.ourplayer.mods })
+			var currentPP = result1.performance.totalPerformance.toFixed(2)
+			var fcPP = result2.performance.totalPerformance.toFixed(2)
+			var ssPP = result3.performance.totalPerformance.toFixed(2)
 			return `${currentStats.hits["100"]}x100/${currentStats.hits["50"]}x50/${currentStats.hits["0"]}xmiss ${parseInt(currentPP)}pp/${parseInt(fcPP)}pp if fc (${parseInt(ssPP)}pp for SS)`
 		}
 	}
