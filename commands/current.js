443d161ac4f45da5760631c59f5844cf83d7ed61
zelda101 <zeldamob2@gmai.com>
2022-11-07 19:23:11 -0600

    fix current mods being incorrect

--- commands/current.js
+++ commands/current.js
@@ -12,9 +12,10 @@ module.exports = {
 		if (data.menu.state == 2 || data.menu.state == 7) {	
 			var currentStats = data.getCurrentStats()
 			var osuFile = await data.getOsuFile()
-			const result1 = await scoreCalculator.calculate({ rulesetId: 0, fileURL: osuFile, count100: currentStats.hits["100"], count50: currentStats.hits["50"], countMiss: currentStats.hits["0"], maxCombo: currentStats.combo.max, mods: currentStats.leaderboard.ourplayer.mods })
-			const result2 = await scoreCalculator.calculate({ rulesetId: 0, fileURL: osuFile, count100: currentStats.hits["100"] + currentStats.hits["0"], count50: currentStats.hits["50"], mods: currentStats.leaderboard.ourplayer.mods })
-			const result3 = await scoreCalculator.calculate({ rulesetId: 0, fileURL: osuFile, mods: currentStats.leaderboard.ourplayer.mods })
+			var mods = currentStats.leaderboard.ourplayer.mods != "" ? currentStats.leaderboard.ourplayer.mods : data.menu.mods.str
+			const result1 = await scoreCalculator.calculate({ rulesetId: 0, fileURL: osuFile, count100: currentStats.hits["100"], count50: currentStats.hits["50"], countMiss: currentStats.hits["0"], maxCombo: currentStats.combo.max, mods: mods })
+			const result2 = await scoreCalculator.calculate({ rulesetId: 0, fileURL: osuFile, count100: currentStats.hits["100"] + currentStats.hits["0"], count50: currentStats.hits["50"], mods: mods })
+			const result3 = await scoreCalculator.calculate({ rulesetId: 0, fileURL: osuFile, mods: mods })
 			var currentPP = result1.performance.totalPerformance.toFixed(2)
 			var fcPP = result2.performance.totalPerformance.toFixed(2)
 			var ssPP = result3.performance.totalPerformance.toFixed(2)
