6307e8c8f01f91ca0f09c14cca8d6c170bbf1c21
zelda101 <zeldamob2@gmai.com>
2022-11-14 19:54:16 -0600

    test

--- websocket.js
+++ websocket.js
@@ -1,8 +1,11 @@
 const config = require("./config.json")
+const { ScoreCalculator } = require("osu-pp-calculator")
+const scoreCalculator = new ScoreCalculator()
 const url = config.osu.gosumemory_address
 const WebSocket = require("ws")
 const { GosuMemory } = require("./classes/gosumemory.js")
 var osuData = {}
+
 
 function startWebsocket() {
 	const connection = new WebSocket(url)
@@ -20,9 +23,16 @@ function startWebsocket() {
 			startWebsocket()
 		}, 3000)
 	})
-	connection.onmessage = (e) => {
+	connection.onmessage = async(e)  => {
 		var data = JSON.parse(e.data)
 		if (data) {
+			var mods = data.gameplay.leaderboard.ourplayer.mods != "" ? data.gameplay.leaderboard.ourplayer.mods : data.menu.mods.str
+			var osuFile = await data.getOsuFile()
+			var result = await scoreCalculator.calculate({ rulesetId: 0, fileURL: osuFile, count100: data.gameplay.hits["100"], count50: data.gameplay.hits["50"], countMiss: data.gameplay.hits["0"], maxCombo: data.gameplay.combo.max, mods: mods })
+			var currentPP = result.performance.totalPerformance.toFixed(2)
+			if (data.menu.state == 2 && data.menu.bm.time.current > osuData.menu.bm.time.current && currentPP > osuData.maxPP) {
+				osuData.maxPP = currentPP
+			}
 			data = new GosuMemory(data)
 			Object.assign(osuData, data)
 		}
