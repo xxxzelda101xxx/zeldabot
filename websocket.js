f0d6dd615fd8e5a76cde66ec0ad06da02b76565a
zelda101 <zeldamob2@gmai.com>
2022-11-14 20:02:13 -0600

    asdf

--- websocket.js
+++ websocket.js
@@ -1,10 +1,19 @@
 const config = require("./config.json")
+const isRemote = config.osu.isRemote
 const { ScoreCalculator } = require("@kionell/osu-pp-calculator")
 const scoreCalculator = new ScoreCalculator()
 const url = config.osu.gosumemory_address
 const WebSocket = require("ws")
 const { GosuMemory } = require("./classes/gosumemory.js")
+const path = require("path")
 var osuData = {}
+var songsFolder
+if (isRemote) {
+	songsFolder = config.osu.osu_files_folder
+}
+else {
+	songsFolder = config.osu.Songs_folder
+}
 
 
 function startWebsocket() {
@@ -27,7 +36,12 @@ function startWebsocket() {
 		var data = JSON.parse(e.data)
 		if (data) {
 			var mods = data.gameplay.leaderboard.ourplayer.mods != "" ? data.gameplay.leaderboard.ourplayer.mods : data.menu.mods.str
-			console.log(osuData.getOsuFile())
+			var osuFile = path.join(songsFolder, data.menu.bm.path.folder, data.menu.bm.path.file)
+			var result = await scoreCalculator.calculate({ rulesetId: 0, fileURL: osuFile, count100: data.gameplay.hits["100"], count50: data.gameplay.hits["50"], countMiss: data.gameplay.hits["0"], maxCombo: data.gameplay.combo.max, mods: mods })
+			var currentPP = result.performance.totalPerformance.toFixed(2)
+			if (data.menu.state == 2 && data.menu.bm.time.current > osuData.menu.bm.time.current && currentPP > osuData.maxPP) {
+				osuData.maxPP = currentPP
+			}
 			data = new GosuMemory(data)
 			Object.assign(osuData, data)
 		}
