9b5825b31ab0747592795fa21301a9779c96fd70
zelda101 <zeldamob2@gmai.com>
2022-11-19 15:36:30 -0600

    a

--- commands/toptenemotes.js
+++ commands/toptenemotes.js
@@ -18,6 +18,7 @@ module.exports = {
 			const user_id = await getUserIdByUsername(msg.toLowerCase().split(" ")[1])
 			if (user_id) {
 				let emotes = await getTopTenEmotesByUserID(context.channelId, user_id)
+				console.log(emotes)
 				let messageToSend = ""
 				for (let i = 0; i < 10; i++) messageToSend += `${emotes[i].emote} `
 				return messageToSend
