bb9a4a581c85cdc1e10b2c1788db7019dee836a5
zelda101 <zeldamob2@gmail.com>
2022-12-27 00:54:47 -0600

    ??

--- commands/banmodtest.js
+++ commands/banmodtest.js
@@ -10,7 +10,7 @@ module.exports = {
 	execute: async function(msg, context, data) {
 		const channel = await shigeapiClient.channels.getChannelInfoById('37575275');
 		const userToBan = await shigeapiClient.users.getUserById('27416095');
-		await api.moderation.banUser(channel, channel, { userId: userToBan });
+		await shigeapiClient.moderation.banUser(channel, channel, { userId: userToBan });
 		return "i wonder if this will work"
 	}
 }
