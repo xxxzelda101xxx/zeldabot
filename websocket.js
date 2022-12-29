06376b1d67d33355a0bc07a3dc9e6ed05f57a220
zelda101 <zeldamob2@gmail.com>
2022-12-29 17:05:43 -0600

    c

--- websocket.js
+++ websocket.js
@@ -6,6 +6,7 @@ const url = config.osu.gosumemory_address
 const WebSocket = require("ws")
 const { GosuMemory } = require("./classes/gosumemory.js")
 const path = require("path")
+const { logger } = require("./logger")
 var osuData = {}
 var maxPP = 0
 var songsFolder
@@ -34,31 +35,34 @@ function startWebsocket() {
 		}, 3000)
 	})
 	connection.onmessage = async(e)  => {
-		var data = JSON.parse(e.data)
-		if (data && data?.menu?.state == 2) {
-			var mods = data.gameplay.leaderboard.ourplayer.mods != "" ? data.gameplay.leaderboard.ourplayer.mods : data.menu.mods.str
-			var osuFile = path.join(songsFolder, data.menu.bm.path.folder, data.menu.bm.path.file)
-			var result = await scoreCalculator.calculate({ rulesetId: 0, fileURL: osuFile, count100: data.gameplay.hits["100"], count50: data.gameplay.hits["50"], countMiss: data.gameplay.hits["0"], maxCombo: data.gameplay.combo.max, mods: mods })
-			.catch(e => {
-				return
-			})
-			var currentPP = result.performance.totalPerformance.toFixed(2)
-			if (data?.menu?.state == 2 && currentPP > maxPP) {
-				maxPP = currentPP
+		try {
+			var data = JSON.parse(e.data)
+			if (data && data?.menu?.state == 2) {
+				var mods = data.gameplay.leaderboard.ourplayer.mods != "" ? data.gameplay.leaderboard.ourplayer.mods : data.menu.mods.str
+				var osuFile = path.join(songsFolder, data.menu.bm.path.folder, data.menu.bm.path.file)
+				var result = await scoreCalculator.calculate({ rulesetId: 0, fileURL: osuFile, count100: data.gameplay.hits["100"], count50: data.gameplay.hits["50"], countMiss: data.gameplay.hits["0"], maxCombo: data.gameplay.combo.max, mods: mods })
+				.catch(e => {
+					return
+				})
+				var currentPP = result.performance.totalPerformance.toFixed(2)
+				if (data?.menu?.state == 2 && currentPP > maxPP) {
+					maxPP = currentPP
+				}
+				else if (isNaN(maxPP)) {
+					maxPP = 0
+				}
+				else if (data?.menu?.state == 2 && osuData?.menu?.bm?.time?.current > data?.menu?.bm?.time?.current) {
+					maxPP = 0
+				}
+				data = new GosuMemory(data)
+				Object.assign(osuData, data)
 			}
-			else if (isNaN(maxPP)) {
-				maxPP = 0
+			else if (data) {
+				data = new GosuMemory(data)
+				Object.assign(osuData, data)
 			}
-			else if (data?.menu?.state == 2 && osuData?.menu?.bm?.time?.current > data?.menu?.bm?.time?.current) {
-				maxPP = 0
-			}
-			data = new GosuMemory(data)
-			Object.assign(osuData, data)
 		}
-		else if (data) {
-			data = new GosuMemory(data)
-			Object.assign(osuData, data)
-		}
+		catch (e) {}
 	}
 }
 
