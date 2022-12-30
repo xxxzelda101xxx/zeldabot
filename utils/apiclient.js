d0b4885412e78d76020fdf94deeb99e640ede756
zelda101 <zeldamob2@gmail.com>
2022-12-29 18:51:04 -0600

    ;

--- utils/apiclient.js
+++ utils/apiclient.js
@@ -1,6 +1,7 @@
 const { zeldaAuthProvider } = require("./authprovider.js")
 var authProvider = zeldaAuthProvider
 const ApiClient = require ("@twurple/api").ApiClient
+const { EventSubWsListener } = require('@twurple/eventsub-ws')
 
 const apiClient = new ApiClient({ authProvider })
 
@@ -9,6 +10,8 @@ exports.apiClient = apiClient
 const { shigeAuthProvider } = require("./authprovider.js")
 authProvider = shigeAuthProvider
 
+const listener = new EventSubWsListener({ authProvider });
 const shigeapiClient = new ApiClient({ authProvider })
 
 exports.shigeapiClient = shigeapiClient
+exports.listener = listener
