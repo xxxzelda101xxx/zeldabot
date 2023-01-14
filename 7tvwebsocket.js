41311c08609ea06ffe85a9829e1ee5d60e405f53
zelda101 <zeldamob2@gmail.com>
2023-01-13 22:57:08 -0600

    a

--- 7tvwebsocket.js
+++ 7tvwebsocket.js
@@ -30,7 +30,12 @@ function start7TVWebsocket(channels) {
 	connection.onmessage = async(e)  => {
         const symbolKey = Reflect.ownKeys(e).find(key => key.toString() === 'Symbol(kData)')
         var data = JSON.parse(e[symbolKey])
-        console.log(data)
+        if (data.d?.body?.pushed) {
+            console.log(data.d?.body?.pushed)
+        }
+        if (data.d?.body?.pulled) {
+            console.log(data.d?.body?.pulled)
+        }
 	}
 }
 
