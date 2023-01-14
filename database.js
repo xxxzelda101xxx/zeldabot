c9b069db4f63df84d450123db9ec5875c2d1ccdb
zelda101 <zeldamob2@gmail.com>
2023-01-13 20:41:13 -0600

    a

--- database.js
+++ database.js
@@ -71,8 +71,8 @@ async function getTopTenEmotesByUserID(channel_id, user_id) {
 }
 
 async function getChannels() {
-	let data = (await db.all("SELECT channel_id FROM channels", [])).map((row) => row.channel_id);
-	console.log(data)
+	//let data = (await db.all("SELECT channel_id FROM channels", [])).map((row) => row.channel_id);
+	//console.log(data)
 	//if (data) return data
 }
 
