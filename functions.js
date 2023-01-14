db895d77d09d7bf2a728a61bc792b60ca7f715d0
zelda101 <zeldamob2@gmail.com>
2023-01-13 22:33:48 -0600

    a

--- functions.js
+++ functions.js
@@ -36,7 +36,7 @@ async function kagamiBanRNG(channel, user) {
 async function get7TVUserIDFromTwitchUserID(twitch_user_id) {
 	var data = await axios.get(`https://7tv.io/v3/users/twitch/${twitch_user_id}`)
 	if (data) {
-		return data.user.id
+		return data
 	}
 }
 
