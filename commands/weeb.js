3c6de4443449250f711170aa40c9e96863fecf97
zelda101 <zeldamob2@gmail.com>
2023-01-27 14:43:48 -0600

    ?

--- commands/weeb.js
+++ commands/weeb.js
@@ -11,10 +11,19 @@ module.exports = {
 		var prompt = msg.split(" ")
 		prompt.shift()
 		prompt = prompt.toString()
+		prompt = prompt.replace(/completely nude/g, "").replace(/nude/g, "").replace(/sex/g, "").replace(/cum/g, "").replace(/anal/g, "").replace(/anus/g, "").replace(/vagina/g, "").replace(/pussy/g, "").replace(/asshole/g, "").replace(/penis/g, "")
 		console.log(prompt)
 		var payload = {
-			"prompt": "maltese puppy",
-			"steps": 5
+			"prompt": prompt,
+			"negative_prompt": "lowres, bad anatomy, ((bad hands)), text, error, ((missing fingers)), cropped, jpeg artifacts, worst quality, low quality, signature, watermark, blurry, deformed, extra ears, deformed, disfigured, mutation, censored, (((nude))), (((sex))), (((cum)))",
+			"steps": 20,
+			"width": 448,
+			"height": 640
 		}
+		const request = await axios.post("http://127.0.0.1:7860/sdapi/v1/txt2img", {
+            prompt: payload,
+        });
+		let image = await request.data.images
+		console.log(image)
 	}
 }
