3b80ec38b1402e033b00f75b2eb480bd44deef75
zelda101 <zeldamob2@gmail.com>
2023-01-27 14:26:56 -0600

    weeb

---
+++ commands/weeb.js
@@ -0,0 +1,18 @@
+const axios = require('axios')
+
+module.exports = {
+	name: "weeb",
+	aliases: [],
+	description: "",
+	canWhisper: true,
+	isOsuCommand: false,
+	isPublic: false,
+	execute: async function(msg, context, data) {
+		var prompt = msg.split(" ").shift()
+		console.log(prompt)
+		var payload = {
+			"prompt": "maltese puppy",
+			"steps": 5
+		}
+	}
+}
