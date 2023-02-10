abbb9b7741d426d3da04af2413436ce8e7e9d881
zelda101 <zeldamob2@gmail.com>
2023-02-10 12:29:28 -0600

    a

---
+++ commands/weebtest.js
@@ -0,0 +1,19 @@
+const axios = require('axios')
+const fs = require('fs')
+const { nanoid } = require('nanoid')
+const config = require('../config.json')
+const url = config.ai.url
+
+module.exports = {
+	name: "weebtest",
+	aliases: [],
+	description: "",
+	canWhisper: false,
+	isOsuCommand: false,
+	isPublic: false,
+	execute: async function(msg, context, data, channel) {
+		let image = await axios.get('https://cdn.donmai.us/sample/93/37/__raiden_shogun_and_yae_miko_genshin_impact_drawn_by_niliu_chahui__sample-9337e38d12eb7df6f3b227fe01fa8b38.jpg', {responseType: 'arraybuffer'});
+		let test = Buffer.from(image.data).toString('base64');
+		console.log(test)
+	}
+}
