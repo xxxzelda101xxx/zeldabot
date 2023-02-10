4147c672a6b11a342eb0f3ed712cef007d613659
zelda101 <zeldamob2@gmail.com>
2023-02-10 12:34:23 -0600

    a

--- commands/weebtest.js
+++ commands/weebtest.js
@@ -12,13 +12,17 @@ module.exports = {
 	isOsuCommand: false,
 	isPublic: false,
 	execute: async function(msg, context, data, channel) {
-		let image = await axios.get('https://cdn.donmai.us/sample/93/37/__raiden_shogun_and_yae_miko_genshin_impact_drawn_by_niliu_chahui__sample-9337e38d12eb7df6f3b227fe01fa8b38.jpg', {responseType: 'arraybuffer'});
+		let file_id = nanoid()
+		let image = await axios.get('https://www.blameseouless.com/files/GRri4m.jpg', {responseType: 'arraybuffer'});
 		let test = Buffer.from(image.data).toString('base64');
 		const payload = {
 			"init_images": test
 		}
 		const request = await axios.post(`${url}/sdapi/v1/txt2img`, payload)
 		let images = await request.data.images
-		var data = await request.data.info
+		fs.writeFileSync(`./images/${file_id}.png`, image[0], 'base64', function(err) {
+			console.log(err);
+		});
+		return `https://blameseouless.com/aiimages/${file_id}.png`
 	}
 }
