5a1a70c5b0596bc93d8104d877545b644cbfb4f3
zelda101 <zeldamob2@gmail.com>
2023-02-10 11:55:39 -0600

    a

--- commands/weeb.js
+++ commands/weeb.js
@@ -70,11 +70,14 @@ module.exports = {
 			var payload2 = {
 				"imageList": []
 			}
-			//for (var i = 0; i < image.length; i++) {
-			//	payload2.imageList.push({ data: image[i], name: `image${i}.png`})
-			//}
+			for (var i = 0; i < image.length; i++) {
+				payload2.imageList.push({ data: image[i], name: `image${i}.png`})
+			}
 			const request2 = await axios.post(`${url}/sdapi/v1/extra-batch-images`, payload2)
-			console.log(request2)
+			.catch(e => {
+				console.log(e)
+			})
+			console.log(request2.data)
 			return "test (check console)"
 		}
 	}
