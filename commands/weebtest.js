595951843c5f813078879422b16af020a4717b3f
zelda101 <zeldamob2@gmail.com>
2023-02-10 12:32:13 -0600

    a

--- commands/weebtest.js
+++ commands/weebtest.js
@@ -14,6 +14,11 @@ module.exports = {
 	execute: async function(msg, context, data, channel) {
 		let image = await axios.get('https://cdn.donmai.us/sample/93/37/__raiden_shogun_and_yae_miko_genshin_impact_drawn_by_niliu_chahui__sample-9337e38d12eb7df6f3b227fe01fa8b38.jpg', {responseType: 'arraybuffer'});
 		let test = Buffer.from(image.data).toString('base64');
-		console.log(test)
+		const payload = {
+			"init_images": test
+		}
+		const request = await axios.post(`${url}/sdapi/v1/txt2img`, payload)
+		let images = await request.data.images
+		var data = await request.data.info
 	}
 }
