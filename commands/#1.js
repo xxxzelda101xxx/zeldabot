a40ef76c3ae63d8d0711f8868fd1d4eda11fdf34
zelda101 <zeldamob2@gmail.com>
2023-02-15 18:52:44 -0600

    fix !#1

--- commands/#1.js
+++ commands/#1.js
@@ -3,12 +3,13 @@ module.exports = {
 	aliases: ["number1"],
 	description: "",
 	canWhisper: true,
-	requiredState: 7,
 	isOsuCommand: true,
 	isPublic: false,
 	execute: async function(msg, context, data) {
-		var numberOneScore = data.getNumber1Score()
-		if (!numberOneScore) return "Not in a map or map is unranked/no local scores."
-		return `#1 score: ${numberOneScore.name} (${numberOneScore.maxCombo}x/${data.getMaxCombo()}x, ${numberOneScore.h100}x100/${numberOneScore.h50}x50/${numberOneScore.h0}xmiss, ${numberOneScore.accuracy}%, +${numberOneScore.mods})`
+		if (data.menu.state == 2 || data.menu.state == 7) {
+			var numberOneScore = data.getNumber1Score()
+			if (!numberOneScore) return "Not in a map or map is unranked/no local scores."
+			return `#1 score: ${numberOneScore.name} (${numberOneScore.maxCombo}x/${data.getMaxCombo()}x, ${numberOneScore.h100}x100/${numberOneScore.h50}x50/${numberOneScore.h0}xmiss, ${numberOneScore.accuracy}%, +${numberOneScore.mods})`
+		}
 	}
 }
