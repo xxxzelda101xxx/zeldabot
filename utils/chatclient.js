6b57539573d219b1411a63bbbad34d8ee2fc0b07
zelda101 <zeldamob2@gmail.com>
2023-02-24 23:45:48 -0600

    a

--- utils/chatclient.js
+++ utils/chatclient.js
@@ -2,6 +2,6 @@ const { authProvider } = require("./authprovider.js")
 const config = require("../config.json")
 
 const ChatClient = require("@twurple/chat").ChatClient
-const chatClient = new ChatClient({ authProvider, channels: config.twitch.channels })
+const chatClient = new ChatClient({ authProvider, channels: config.twitch.channels, isAlwaysMod: true })
 
 exports.chatClient = chatClient
