8b7e00f013146cace7433ad5e188e3dadbdafdf8
zelda101 <zeldamob2@gmail.com>
2023-02-27 13:01:50 -0600

    dejj

--- commands/current.js
+++ commands/current.js
@@ -1,9 +1,8 @@
 const { ScoreCalculator } = require("@kionell/osu-pp-calculator")
 const { getMaxPP } = require("../websocket.js")
 const scoreCalculator = new ScoreCalculator()
+const unsubmittedDownloadPath = "https://blameseouless.com/osufiles/"
 const path = require("path")
-const config = require("../config.json")
-const songsFolder = config.osu.Songs_folder
 
 module.exports = {
 	name: "current",
@@ -15,16 +14,15 @@ module.exports = {
 	execute: async function(msg, context, data, args) {
 		if (data.menu.state == 2 || data.menu.state == 7) {	
 			var currentStats = data.getCurrentStats()
-			var osuFile = path.join(songsFolder, data.menu.bm.path.folder, data.menu.bm.path.file)
-			console.log(osuFile)
+			var osuFile = path.join(data.menu.bm.path.folder, data.menu.bm.path.file)
 			var mods = currentStats.leaderboard.ourplayer.mods != "" ? currentStats.leaderboard.ourplayer.mods : data.menu.mods.str
-			const result1 = await scoreCalculator.calculate({ rulesetId: 0, fileURL: osuFile, count100: currentStats.hits["100"], count50: currentStats.hits["50"], countMiss: currentStats.hits["0"], maxCombo: currentStats.combo.max, mods: mods })
+			const result1 = await scoreCalculator.calculate({ rulesetId: 0, fileURL: unsubmittedDownloadPath + osuFile, count100: currentStats.hits["100"], count50: currentStats.hits["50"], countMiss: currentStats.hits["0"], maxCombo: currentStats.combo.max, mods: mods })
 			.catch(e => {
 				if (e.code == "ENOENT") throw new Error('Failed to calculate. Beatmap not found.')
 				else throw new Error(e)
 			})
-			const result2 = await scoreCalculator.calculate({ rulesetId: 0, fileURL: osuFile, count100: currentStats.hits["100"] + currentStats.hits["0"], count50: currentStats.hits["50"], mods: mods })
-			const result3 = await scoreCalculator.calculate({ rulesetId: 0, fileURL: osuFile, mods: mods })
+			const result2 = await scoreCalculator.calculate({ rulesetId: 0, fileURL: unsubmittedDownloadPath + osuFile, count100: currentStats.hits["100"] + currentStats.hits["0"], count50: currentStats.hits["50"], mods: mods })
+			const result3 = await scoreCalculator.calculate({ rulesetId: 0, fileURL: unsubmittedDownloadPath + osuFile, mods: mods })
 			var currentPP = result1.performance.totalPerformance.toFixed(2)
 			var fcPP = result2.performance.totalPerformance.toFixed(2)
 			var ssPP = result3.performance.totalPerformance.toFixed(2)
--- websocket.js
+++ websocket.js
@@ -6,6 +6,7 @@ const WebSocket = require("ws")
 const { GosuMemory } = require("./classes/gosumemory.js")
 const path = require("path")
 const { logger } = require("./logger")
+const unsubmittedDownloadPath = "https://blameseouless.com/osufiles/"
 var osuData = {}
 var maxPP = 0
 const songsFolder = config.osu.Songs_folder
@@ -32,8 +33,8 @@ function startWebsocket() {
 		var data = JSON.parse(e.data)
 		if (data) {
 			var mods = data.gameplay.leaderboard.ourplayer.mods != "" ? data.gameplay.leaderboard.ourplayer.mods : data.menu.mods.str
-			var osuFile = path.join(songsFolder, data.menu.bm.path.folder, data.menu.bm.path.file)
-			var result = await scoreCalculator.calculate({ rulesetId: 0, fileURL: osuFile, count100: data.gameplay.hits["100"], count50: data.gameplay.hits["50"], countMiss: data.gameplay.hits["0"], maxCombo: data.gameplay.combo.max, mods: mods })
+			var osuFile = path.join(data.menu.bm.path.folder, data.menu.bm.path.file)
+			var result = await scoreCalculator.calculate({ rulesetId: 0, fileURL: unsubmittedDownloadPath + osuFile, count100: data.gameplay.hits["100"], count50: data.gameplay.hits["50"], countMiss: data.gameplay.hits["0"], maxCombo: data.gameplay.combo.max, mods: mods })
 			.catch(e => {
 				if (e.code != "ENOENT") {
 					console.log(e)
