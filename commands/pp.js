c8b0a008cf47f2fd01ee6cbeb55322d639da60d7
zelda101 <zeldamob2@gmail.com>
2023-03-02 14:52:16 -0600

    a

--- commands/pp.js
+++ commands/pp.js
@@ -51,9 +51,7 @@ module.exports = {
 					})
 				}
 			}
-			console.log(accuracy)
-			if (msg.indexOf("+") < 0) accuracy = Number(msg.toLowerCase().split(" ")[1]).toFixed(2)
-			console.log(accuracy)
+			//if (msg.indexOf("+") < 0) accuracy = Number(msg.toLowerCase().split(" ")[1]).toFixed(2)
 			if (accuracy > 100) accuracy = 100
 			if (accuracy < 33.33) accuracy = 33.33
 			var osuFile = path.join(songsFolder, data.menu.bm.path.folder, data.menu.bm.path.file)
