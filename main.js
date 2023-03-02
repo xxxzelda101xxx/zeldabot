6d3a96c75c7f9d50c0509d006092ce7fdc165882
zelda101 <zeldamob2@gmail.com>
2023-03-02 13:50:51 -0600

    ?

--- main.js
+++ main.js
@@ -20,7 +20,9 @@ async function main() {
 	startSevenTVWebsocket(channels)
 	await chatClient.connect()
 	await listener.start()
-	if (config.twitch.is_official_bot) await shigeapiClient.eventSub.deleteAllSubscriptions()
+	if (config.twitch.is_official_bot) {
+		await shigeapiClient.eventSub.deleteAllSubscriptions()
+	}
 	chatClient.onRegister(() => {
 		logger.info("Connected to Twitch!")
 	})
