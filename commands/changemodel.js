2c50f3fec93ddbe9a7ce27d9c998035b25b5a52a
zelda101 <zeldamob2@gmail.com>
2023-03-04 00:16:44 -0600

    test

--- commands/changemodel.js
+++ commands/changemodel.js
@@ -12,15 +12,23 @@ module.exports = {
 	execute: async function(msg, context, args) {
         const request = await axios.get(`${url}/sdapi/v1/sd-models`)
         var data = request.data
-        if (args.length == 1) {
-            var modelsArray = []
-            for (var i = 0; i < data.length; i++) {
-                modelsArray.push(`[${i + 1}] ${data[i].model_name}`)
+        var modelsArray = []
+        var modelMatchIndex = -1
+        for (var i = 0; i < data.length; i++) {
+            modelsArray.push(`[${i + 1}] ${data[i].model_name}`)
+            if (data[i].model_name.indexOf(args[1] > -1)) {
+                modelMatchIndex = i
             }
+        }
+        if (args.length == 1) {
             return modelsArray
         }
         else if (args.length == 2 && !isNaN(args[1]) && args[1] > 0 && args[1] <= data.length) {
             const request2 = await axios.post(`${url}/sdapi/v1/options`, { sd_model_checkpoint: data[args[1] - 1].title})
+            return `Changed model to ${data[args[1] - 1].model_name}`
+        }
+        else if (args.length == 2 && modelMatchIndex > -1) {
+            const request2 = await axios.post(`${url}/sdapi/v1/options`, { sd_model_checkpoint: data[modelMatchIndex].title})
             return `Changed model to ${data[args[1] - 1].model_name}`
         }
 		return "dejj"
