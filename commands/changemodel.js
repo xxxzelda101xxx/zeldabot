ed8108a86d9a0f3c8f722ff7c374ff225cba5f84
zelda101 <zeldamob2@gmail.com>
2023-03-04 00:02:05 -0600

    test

--- commands/changemodel.js
+++ commands/changemodel.js
@@ -4,21 +4,25 @@ const url = config.ai.url
 
 module.exports = {
 	name: "changemodel",
-	aliases: [],
+	aliases: ["cm"],
 	description: "Changes the model for stable diffusion.",
 	canWhisper: false,
 	isOsuCommand: false,
 	isPublic: false,
 	execute: async function(msg, context, args) {
+        const request = await axios.get(`${url}/sdapi/v1/sd-models`)
+        var data = request.data
         if (args.length == 1) {
-            const request = await axios.get(`${url}/sdapi/v1/sd-models`)
-            var data = request.data
             var modelsArray = []
-            for (var i = 0; i < request.data.length; i++) {
-                modelsArray.push(`[${i + 1}] ${request.data[i].model_name}`)
+            for (var i = 0; i < data.length; i++) {
+                modelsArray.push(`[${i + 1}] ${data[i].model_name}`)
             }
             return modelsArray
         }
-		return ""
+        else if (args.length == 2 && !isNaN(args[1]) && args[1] > 0 && args[1] <= data.length) {
+            const request2 = await axios.post(`${url}/sdapi/v1/options`, { sd_model_checkpoint: data[args[1] - 1].title})
+            return `Changed model to ${data[args[1] - 1].model_name}`
+        }
+		return "dejj"
 	}
 }
