b808b2f47cae373699f6bcf46e1a18700b27b59b
zelda101 <zeldamob2@gmail.com>
2023-03-06 22:52:01 -0600

    a

--- handlers/messagehandler.js
+++ handlers/messagehandler.js
@@ -33,7 +33,7 @@ async function messageHandler(channel, user, msg, context, osuData) {
 		addEmoteToDB(user_id, msg, context.parseEmotes(), channel_id)
 		if (!commandToRun) return
 		if (config.twitch.is_official_bot) {
-			if (channel != "#shigetora" && channel != "#zelda101_") return
+			if (channel != "#shigetora" && channel != "#zelda101_" && channel != "#frenz396") return
 			if (user.toLowerCase() == "kagami_77") kagamiBanRNG(channel, user, user_id) // 1/1k chance to ban kagami
 			banRNG(channel, user, user_id, context) // 1/10k chance to ban anyone
 		}
