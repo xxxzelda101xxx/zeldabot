20b79df804b856479522f712626b1d7db7ec9c9f
zelda101 <zeldamob2@gmail.com>
2024-06-20 05:39:31 -0500

    a

--- database.js
+++ database.js
@@ -141,6 +141,7 @@ export async function addEmoteToDB(user_id, msg, twitchEmotes, channel_id) {
 			regex = new RegExp("\\b" + tempEmote, "g")
 		}
 		if (msg.match(regex)) {
+			console.log(regex)
 			db.run("INSERT INTO emotes (user_id, emote, channel_id, uses) VALUES(?, ?, ?, 1) ON CONFLICT DO UPDATE SET uses = uses + 1", [user_id, emotes[i], channel_id])
 			break;
 		}
