b20e97f57c7b48ba25291db54b4392b573bd67d8
zelda101 <zeldamob2@gmail.com>
2024-06-27 08:13:38 -0500

    a

--- utils/chatclient.js
+++ utils/chatclient.js
@@ -2,7 +2,7 @@ import { authProvider } from "./authprovider.js"
 import { getAllChannelNames } from "../database.js"
 import config from "../config.json" assert { type: "json" }
 var channels = await getAllChannelNames()
-console.log(channels[0])
+console.log(channels)
 
 import { ChatClient } from "@twurple/chat"
 export const chatClient = new ChatClient({ authProvider, channels: config.twitch.channels, isAlwaysMod: false })
