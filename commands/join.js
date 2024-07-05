31824dcc51b46f3d48c9f0fe1cfc72a7e3308d35
zelda101 <zeldamob2@gmail.com>
2024-07-05 02:40:56 -0500

    a

--- commands/join.js
+++ commands/join.js
@@ -15,7 +15,7 @@ export default {
 			chatClient.join(context.userInfo.userName)
 			return `zeldabot has joined #${context.userInfo.userName}. By default the bot is muted, use !unmute in YOUR chat to unmute zeldabot`
 		}
-		if (channelToJoin && config.twitch.admins.indexOf(context.userInfo.userName) > 0) {
+		else if (channelToJoin && config.twitch.admins.indexOf(context.userInfo.userName) > 0) {
 			var channel = await apiClient.users.getUserByName(channelToJoin)
 			if (channel) {
 				await getChannelData(channel.name)
@@ -24,5 +24,6 @@ export default {
 			}
 			else return "Channel doesn't exist!"
 		}
+		else return "You dont have permission to do that!"
 	}
 }
