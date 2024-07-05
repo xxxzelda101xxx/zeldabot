b6255da9bf94c4a74f8db5f384161f0a3c5944c6
zelda101 <zeldamob2@gmail.com>
2024-07-05 10:00:21 -0500

    a

--- handlers/messagehandler.js
+++ handlers/messagehandler.js
@@ -28,7 +28,7 @@ async function messageHandler(channel, user, msg, context, osuData) {
 		addToDB(user_id, channel_id)
 		addEmoteToDB(user_id, msg, parseChatMessage(msg, context.emoteOffsets), channel_id)
 		const isBotMuted = await isBotMutedInChannel(channel)
-		if (isBotMuted && command != "unmute") return
+		if (isBotMuted && command != "botunmute") return
 		//if (user.toLowerCase() == "kagami_77") kagamiBanRNG(channel, user, user_id, context) // 1/1k chance to ban kagami
 		//banRNG(channel, user, user_id, context) // 1/10k chance to ban anyone
 		if (!commandToRun) {
