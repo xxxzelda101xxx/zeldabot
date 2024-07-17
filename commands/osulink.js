5ef45f00535c5bc767448e31dfba084e8b68830c
zelda101 <zeldamob2@gmail.com>
2024-07-16 23:03:35 -0500

    a

--- commands/osulink.js
+++ commands/osulink.js
@@ -1,4 +1,5 @@
 import * as osu from "osu-api-v2-js"
+import config from '../config.json' assert { type: "json" }
 import { setOsuUsername } from "../database.js"
 
 
@@ -8,6 +9,7 @@ export default {
 	canWhisper: true,
 	execute: async function(msg, context, data, args) {
         const api = await osu.API.createAsync({id: config.osu.client_id, secret: config.osu.client_secret})
+        if (msg.toLowerCase().split(" ").length == 1) return "Please specify a username!"
         const username = msg.toLowerCase().split(" ")[1]
         const user = await api.getUser(username)
         console.log(user)
