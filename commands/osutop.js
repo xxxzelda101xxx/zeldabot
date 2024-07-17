a346f51d32c589b61204983d568bb5f5d4f6b6e8
zelda101 <zeldamob2@gmail.com>
2024-07-16 23:23:21 -0500

    a

--- commands/osutop.js
+++ commands/osutop.js
@@ -7,17 +7,16 @@ export default {
 	description: "",
 	canWhisper: true,
 	execute: async function(msg, context, data, args) {
-        const api = await osu.API.createAsync({id: config.osu.client_id, secret: config.osu.client_secret})
-        var username
-        if (msg.toLowerCase().split(" ").length == 1) username = await getOsuUsername(context.userInfo.userId)
-        else username = msg.toLowerCase().split(" ")[1]
-        const user = await api.getUser(username)
-        const score = (await api.getUserScores(user, "best", osu.Ruleset.osu, {lazer: false}, {limit: 2}))[1] // Specifying the Ruleset is optional
-        const beatmapDifficulty = await api.getBeatmapDifficultyAttributesOsu(score.beatmap, score.mods) // Specifying the mods so the SR is adapted to them
-    
-        const x = `${score.beatmapset.artist} - ${score.beatmapset.title} [${score.beatmap.version}]`
-        const y = `+${score.mods.toString()} ${(score.accuracy * 100).toFixed(2)}% (${beatmapDifficulty.star_rating.toFixed(2)}*)`
-        console.log(`${username}'s top play is on: ${x} ${y}`)
-        // Doomsday fanboy's top play is on: Yamajet feat. Hiura Masako - Sunglow [Harmony] +DT (8.72*)
+                const api = await osu.API.createAsync({id: config.osu.client_id, secret: config.osu.client_secret})
+                var username
+                if (msg.toLowerCase().split(" ").length == 1) username = await getOsuUsername(context.userInfo.userId)
+                else username = msg.toLowerCase().split(" ")[1]
+                const user = await api.getUser(username)
+                const score = (await api.getUserScores(user, "best", osu.Ruleset.osu, {lazer: false}, {limit: 1}))[0] // Specifying the Ruleset is optional
+                const beatmapDifficulty = await api.getBeatmapDifficultyAttributesOsu(score.beatmap, score.mods) // Specifying the mods so the SR is adapted to them
+        
+                const x = `${score.beatmapset.artist} - ${score.beatmapset.title} [${score.beatmap.version}]`
+                const y = `+${score.mods.toString()} ${(score.accuracy * 100).toFixed(2)}% (${beatmapDifficulty.star_rating.toFixed(2)}*)`
+                return `${username}'s top play is on: ${x} ${y}`
 	}
 }
