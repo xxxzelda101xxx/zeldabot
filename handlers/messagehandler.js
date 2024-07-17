7d7c24e24f4f26bec01ea9860fec41c01ae86ddc
zelda101 <zeldamob2@gmail.com>
2024-07-17 04:24:43 -0500

    a

--- handlers/messagehandler.js
+++ handlers/messagehandler.js
@@ -101,7 +101,7 @@ async function runCommand(command, channel, msg, context, args) {
 		logger.error(`Command ${command.name} Failed: ${e}`)
 		console.log(e)
 		if (e?.status_code == 404) chatClient.say(channel, "That username doesn't exist!", { replyTo: context })
-		else if (e?.getEndPoint.indexOf("beatmaps") > -1) chatClient.say(channel, "User doesn't have a score on this map!", { replyTo: context })
+		else if (e?.endpoint.indexOf("beatmaps") > -1) chatClient.say(channel, "User doesn't have a score on this map!", { replyTo: context })
 		else chatClient.say(channel, e.toString(), { replyTo: context })
 	}
 }
