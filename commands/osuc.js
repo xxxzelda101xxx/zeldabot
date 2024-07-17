fd72c2183cbcef53ecfbee10ea741c4649e332c2
zelda101 <zeldamob2@gmail.com>
2024-07-17 04:33:51 -0500

    a

--- commands/osuc.js
+++ commands/osuc.js
@@ -15,9 +15,9 @@ export default {
                 if (username == "" || username == null) return "Please set your osu! username using !osulink <osu! username>!"
                 const user = await api.getUser(username)
                 if (!lastMap[context.channelId]) return "Last beatmap not found."
-                const score = (await api.getBeatmapUserScore(lastMap[context.channelId], user))
+                const score = (await api.getBeatmapUserScores(lastMap[context.channelId], user))
                 console.log(score)
-                const beatmapDifficulty = await api.getBeatmapDifficultyAttributesOsu(lastMap[context.channelId], score.mods) // Specifying the mods so the SR is adapted to them
+                const beatmapDifficulty = await api.getBeatmapDifficultyAttributesOsu(score.beatmap, score.mods) // Specifying the mods so the SR is adapted to them
                 var pp
                 if (score.pp != null) pp = `${score.pp.toFixed(2)}pp `
                 else pp = ""
