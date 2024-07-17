4b2065c057736b78b2ac002e1c171ac236234251
zelda101 <zeldamob2@gmail.com>
2024-07-16 23:39:34 -0500

    a

--- commands/osutop.js
+++ commands/osutop.js
@@ -8,13 +8,13 @@ export default {
 	canWhisper: true,
 	execute: async function(msg, context, data, args) {
                 const api = await osu.API.createAsync({id: config.osu.client_id, secret: config.osu.client_secret})
+                var scoreIndex = parseInt(msg.substring(7).trim().toLowerCase().split(" ")[0])
+                if (isNaN(scoreIndex)) scoreIndex = 1
                 var username
                 if (msg.toLowerCase().split(" ").length == 1) username = await getOsuUsername(context.userInfo.userId)
                 else username = msg.toLowerCase().split(" ")[1]
+                if (username == "" || username == NULL) return "Please set your osu! username using !osulink <osu! username>!"
                 const user = await api.getUser(username)
-                var scoreIndex = parseInt(msg.substring(7).trim().toLowerCase().split(" ")[0])
-                console.log(scoreIndex)
-                if (isNaN(scoreIndex)) scoreIndex = 1
                 if (scoreIndex > 100) return "You can't have more than 100 top plays!"
                 const score = (await api.getUserScores(user, "best", osu.Ruleset.osu, {lazer: false}, {limit: scoreIndex}))[scoreIndex - 1]
                 const beatmapDifficulty = await api.getBeatmapDifficultyAttributesOsu(score.beatmap, score.mods) // Specifying the mods so the SR is adapted to them
