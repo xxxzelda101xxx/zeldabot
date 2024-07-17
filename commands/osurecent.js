f85b297f3c3828566c67ceeec5b7101bccda598b
zelda101 <zeldamob2@gmail.com>
2024-07-17 03:41:55 -0500

    a

--- commands/osurecent.js
+++ commands/osurecent.js
@@ -8,7 +8,7 @@ export default {
 	canWhisper: true,
 	execute: async function(msg, context, data, args) {
         const api = await osu.API.createAsync({id: config.osu.client_id, secret: config.osu.client_secret})
-        var scoreIndex = parseInt(msg.substring(7).trim().toLowerCase().split(" ")[0])
+        var scoreIndex = parseInt(msg.substring(10).trim().toLowerCase().split(" ")[0])
         if (isNaN(scoreIndex)) scoreIndex = 1
         var username
         if (msg.toLowerCase().split(" ").length == 1) username = await getOsuUsername(context.userInfo.userId)
