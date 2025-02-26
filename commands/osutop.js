3a808fcc5f4040781fddb47b7f8729872eae59e2
zelda101 <zeldamob2@gmail.com>
2025-02-26 01:08:09 -0600

    a

--- commands/osutop.js
+++ commands/osutop.js
@@ -12,6 +12,13 @@ export default {
         var scoreIndex
         if (alias) scoreIndex = parseInt(msg.substring(alias.length).trim().toLowerCase().split(" ")[0])
         else scoreIndex = parseInt(msg.substring(5).trim().toLowerCase().split(" ")[0])
+        console.log(msg)
+        console.log(msg.substring(1).trim().toLowerCase().split(" ")[0])
+        console.log(msg.substring(2).trim().toLowerCase().split(" ")[0])
+        console.log(msg.substring(3).trim().toLowerCase().split(" ")[0])
+        console.log(msg.substring(4).trim().toLowerCase().split(" ")[0])
+        console.log(msg.substring(5).trim().toLowerCase().split(" ")[0])
+        console.log(msg.substring(6).trim().toLowerCase().split(" ")[0])
         if (isNaN(scoreIndex)) scoreIndex = 1
         var username
         if (msg.toLowerCase().split(" ").length == 1) username = await getOsuUsername(context.userInfo.userId)
